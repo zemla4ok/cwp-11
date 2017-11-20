1889251e5e5b425278c691c719c0103797a3dad1
zemla4ok <dima-vital@live.com>
2017-11-20 22:32:16 +0300

    readall

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+let films = require('./top250.json');
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/api/films/readall', (req, res) => {
+  res.send(films);    
+});
+
+app.get('/api/films/read', (req, res) => {
+ 
 });
 
 app.listen(3000, () => {
