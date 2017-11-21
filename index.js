332cd9bd2254b21c6268b12ccfb9725fffd7c59d
zemla4ok <dima-vital@live.com>
2017-11-21 23:29:34 +0300

    deleting actor by id

--- index.js
+++ index.js
@@ -12,7 +12,21 @@ const idErr = {code: 400, message: 'Where is id?'}
 const invId = {code: 400, message: 'invalid id'}
 const invBirth = {code: 400, message: 'invalid birth'}
 
-
+app.post('/api/actors/delete', (req, res) => {
+  let r= req.body;
+  if(!r.id){
+    res.json(idErr);
+    return;
+  }
+  let id = parseInt(r.id);
+  let index = actors.findIndex(i => i.id == id);
+  if(index < 0){
+    res.json(invId);
+    return;
+  }
+  actors.splice(index, 1);
+  res.json(actors);
+})
 
 app.post('/api/actors/update', (req, res) => {
   let r = req.body;
